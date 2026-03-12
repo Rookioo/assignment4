b27aac7e9b70244cdaa85eb5d8859214eb6feb2e
Rookioo <andersond5235@my.uwstout.edu>
2026-03-12 14:31:37 -0500

    Completed and tested requirements: 4: (input and output redirection) 5) The shell should be able to handle simple piping operations (single piping)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 // Programming Project 4 (System Programming and Linux Shell)
-// Used WSL and programmed in VSCode with Github connected
+// Framework: WSL VSCode
+// Version Management: Github
 // Programmers: Joe / Dustin
 #include <iostream>
 #include <vector>
@@ -17,24 +18,24 @@ using namespace std;
 
 
 // functions
- vector<string>getHistory(string fileName) {
- 	// Use shared C function from assignment
-	vector<string> hisVec;
+vector<string>getHistory(string fileName) {
+// Use shared C function from assignment
+vector<string> hisVec;
 
-   ifstream inFile;
-   inFile.open( fileName);
-   if (!inFile.is_open()) {
+ifstream inFile;
+inFile.open( fileName);
+if (!inFile.is_open()) {
 	// file not found because this is the first command
-      return hisVec;
-   }
-   string line;
-   while (getline(inFile, line)) {
-      if (!line.empty()) {
-         hisVec.push_back(line);
-      }
-   }
 	return hisVec;
- }
+}
+string line;
+while (getline(inFile, line)) {
+	if (!line.empty()) {
+		hisVec.push_back(line);
+	}
+}
+	return hisVec;
+}
 
 
 // prints current working directory
@@ -64,35 +65,138 @@ void updatehisFile(const string& fileName, string line) {
    outFile << line << "\n";
 }
 
+// Handling the < and > operators
+void handleRedirection(vector<char*>& args) {
+    for (int i = 0; args[i] != NULL; i++) {
+        // Output Redirection
+        if (strcmp(args[i], ">") == 0) {
+            int fd = open(args[i+1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
+            if (fd < 0) { perror("open"); exit(1); }
+            dup2(fd, STDOUT_FILENO); // Redirect STDOUT to file
+            close(fd);
+            args[i] = NULL; // Hide '>' and filename from execvp
+            break;
+        } 
+        // Input Redirection
+        else if (strcmp(args[i], "<") == 0) {
+            int fd = open(args[i+1], O_RDONLY);
+            if (fd < 0) { perror("open"); exit(1); }
+            dup2(fd, STDIN_FILENO); // Redirect STDIN from file
+            close(fd);
+            args[i] = NULL;
+            break;
+        }
+    }
+}
+
+// handling piping
+void handlePiping(string line) {
+    size_t pipePos = line.find('|');
+    string part1 = line.substr(0, pipePos);
+    string part2 = line.substr(pipePos + 1);
+
+    int pipefd[2];
+    pipe(pipefd); // fd[0] is read, fd[1] is write
+
+    // Forking first child for the left side of the pipe
+    if (fork() == 0) {
+        // Child 1: Send output to the pipe
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[0]);
+        close(pipefd[1]);
+
+        // tokenize cmd1 here
+        vector<char*> args1;
+        char* temp = strdup(part1.c_str());
+        char* token = strtok(temp, " ");
+        while(token) { args1.push_back(token); token = strtok(NULL, " "); }
+        args1.push_back(NULL);
+        
+        execvp(args1[0], args1.data());
+        exit(1);
+    }
+
+    // Forking second child for the right side
+    if (fork() == 0) {
+        dup2(pipefd[0], STDIN_FILENO);
+        close(pipefd[1]);
+        close(pipefd[0]);
+
+        vector<char*> args2;
+        char* temp = strdup(part2.c_str());
+        char* token = strtok(temp, " ");
+        while(token) { args2.push_back(token); token = strtok(NULL, " "); }
+        args2.push_back(NULL);
+
+        execvp(args2[0], args2.data());
+        exit(1);
+    }
+
+    // Parent has to close both and wait so the prompt doesn't get messed up    close(pipefd[0]);
+    close(pipefd[1]);
+    wait(NULL);
+    wait(NULL);
+}
+
 int main(){
-    
     vector<string>hisVec = getHistory("hit.txt");
 	string line;
 
 	while(true){
 	    cout<< "\n" << printWrkDir()<<"$ ";
 	    getline(cin,line);
-	 	hisVec.push_back(line); //
+	 	hisVec.push_back(line);
 		updatehisFile("hit.txt", line);
 
 		if(line == "exit") exit(0);
-	 	else if (line == "pwd") cout<<printWrkDir();
-	 	else if(line == "history") printHis(hisVec);
-	// 	elseif(stdOutRed(line)>-1) {
-	// 		// part2 Proj3
-	// 	}
-	// 	elseif(stdInRed(line)) {
-	// 		cmd<f;
-	// 		execvp
-	// 	}
-	// 	elseif(hasPiping(line) > -1) {
-	// 		// full twochildren_pipe_dup2-1.cpp
-	// 		// copy paste code except use only one input
-	// 	}
-	// 	else {
-	// 		// ls, ps, ./a.out, java j
-	// 		execvp(cmd[0], cmd)
-	// 	}
+	 	else if (line == "pwd") {
+            cout<<printWrkDir();
+            continue; // Prevent double execution
+        }
+	 	else if(line == "history") {
+            printHis(hisVec);
+            continue; // Prevent shell error
+        }
+
+        // Tokenizing for external commands
+		vector<char*> args;
+		char* temp = strdup(line.c_str()); 
+		char* token = strtok(temp, " ");
+		while (token != NULL) {
+			args.push_back(token);
+			token = strtok(NULL, " ");
+		}
+		args.push_back(NULL);
+
+		if (args[0] == NULL) continue;
+
+        bool hasPipe = false;
+
+        for (int i = 0; args[i] != NULL; i++) {
+            if (strcmp(args[i], "|") == 0) {
+                hasPipe = true;
+                break;
+            }
+        }
+
+        if (hasPipe) {
+        // Piping
+        handlePiping(line); 
+        } else {
+            // Redirection
+            pid_t pid = fork();
+            if (pid == 0) {
+                // In Child Process
+                handleRedirection(args);
+                if (execvp(args[0], args.data()) == -1) {
+                    perror("Shell Error");
+                }
+                exit(1); // Exit if child fails
+            } else {
+                // Parent wait for child to finis
+                wait(NULL);
+            }
+        }
 	}
     return 0;
 }
